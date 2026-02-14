3c242a4b67a9838c1b5a1e838523c53f2d29159f
Jaideep-AJ <142711106+Jaideep-AJ@users.noreply.github.com>
2026-02-14 14:36:06 +0530

    Fix delay bug

--- 02. PWM Blink/02. PWM Blink.ino
+++ 02. PWM Blink/02. PWM Blink.ino
@@ -23,15 +23,15 @@ void loop()
 	int brightness = 50;
 
 	// delay time in milliseconds
-	int delay = 1000; 
+	int delay_ms = 1000; 
 
 	// analogWrite argument must be between 0 and 255
 	// so convert percentage to value between 0 and 255
 	analogWrite(output, 255*brightness/100);    
 	//Finally outputting the read value on pin-9 fading LED
 
-	delay(delay); // wait for delay milliseconds
+	delay(delay_ms); // wait for delay milliseconds
 	analogWrite(output, 0); // turn the LED off
-	delay(delay); // wait for delay milliseconds
+	delay(delay_ms); // wait for delay milliseconds
 
 }  
